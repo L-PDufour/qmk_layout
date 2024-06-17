2532a9c5cc069aff351013b772c435bbe56ff2ba
L-PDufour <leonpierre.dufour@gmail.com>
2024-06-17 14:02:12 -0400

    initial commit

---
+++ rules.mk
@@ -0,0 +1,13 @@
+MOUSEKEY_ENABLE = no    # Mouse keys(+1482)
+EXTRAKEY_ENABLE = yes   # Audio control and System control(+470)
+OLED_DRIVER_ENABLE = yes   # 127X32 OLED Screen
+LTO_ENABLE = yes   # Link Time Optimization
+VIA_ENABLE = no    # Configuring keyboard with VIA
+MIDI_ENABLE = no    # MIDI controls
+WPM_ENABLE = yes   # WPM Calculation
+CONSOLE_ENABLE = yes
+RGB_MATRIX_ENABLE       = no   # Matrix LEDs
+
+CFLAGS                 += -flto
+EXTRAFLAGS             += -flto
+
